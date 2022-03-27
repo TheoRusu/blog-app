bc8ab9dc443572caa07251e1dbfd33cee416cfbc
Theodore Rusu <trusu1@myseneca.ca>
2022-03-26 23:38:59 -0400

    sixth commit

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("./dist/angular-blog-app"));
+app.use(express.static("./dist/bti425-a3-theorusu"));
 
 app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "./dist/angular-blog-app" })
+  res.sendFile("index.html", { root: "./dist/bti425-a3-theorusu" })
 );
 
 // Start the app by listening on the default Heroku port
