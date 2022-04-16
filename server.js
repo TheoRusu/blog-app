60bb779302df650e7617f738e4b3ff0049ce0890
Theodore Rusu <trusu1@myseneca.ca>
2022-04-15 21:06:25 -0400

    updates to deploy to herokut

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("./dist/bti425-a3-theorusu"));
+app.use(express.static("./dist/bti425-a4-theorusu"));
 
 app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "./dist/bti425-a3-theorusu" })
+  res.sendFile("index.html", { root: "./dist/bti425-a4-theorusu" })
 );
 
 // Start the app by listening on the default Heroku port
