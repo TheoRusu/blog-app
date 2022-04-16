15990649a0a1dcea035a6fb4a373cc221413e42d
Theodore Rusu <trusu1@myseneca.ca>
2022-04-15 23:05:08 -0400

    Changed Date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@
  * assignment has been copied manually or electronically from any other source (including web sites) or
  * distributed to other students.
  *
- * Name: Theo Rusu Student ID: 101613206 Date: March 26, 2022
+ * Name: Theo Rusu Student ID: 101613206 Date: April 15, 2022
  * Heroku Link: https://blog-app-theorusu.herokuapp.com/home
  ********************************************************************************/
 import { Component } from '@angular/core';
